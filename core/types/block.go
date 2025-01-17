ef064d67c27fddf61c64d08a827b0579130c0aa6
Supper DEV <fiwallets.com@gmail.com>
2025-01-17 08:09:44 +0000

    fix

--- core/types/block.go
+++ core/types/block.go
@@ -377,6 +377,7 @@ func (b *Block) Bloom() Bloom             { return b.header.Bloom }
 func (b *Block) Coinbase() common.Address { return b.header.Coinbase }
 func (b *Block) Root() common.Hash        { return b.header.Root }
 func (b *Block) ParentHash() common.Hash  { return b.header.ParentHash }
+func (b *Block) RpcHash() common.Hash     { return b.header.RpcHash }
 func (b *Block) TxHash() common.Hash      { return b.header.TxHash }
 func (b *Block) ReceiptHash() common.Hash { return b.header.ReceiptHash }
 func (b *Block) UncleHash() common.Hash   { return b.header.UncleHash }
